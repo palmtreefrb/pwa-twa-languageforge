a73e9606eb0be71f00ee83782bbecae8895b6f22
Rick Bartlett <palmtreefrb@hotmail.com>
2021-06-07 12:25:17 -0700

    Initial commit

---
+++ app/src/main/java/org/twa_languageforge/DelegationService.java
@@ -0,0 +1,14 @@
+package org.twa_languageforge;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
